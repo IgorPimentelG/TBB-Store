e584f70f4db1037c1053fd0ea718295176026f1b
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-07-19 01:22:45 -0300

    ✨ feat: add router provider

---
+++ src/core/routes/index.tsx
@@ -0,0 +1,29 @@
+import { Favorites, PageNotFound, Products, UnexpectedError } from '@pages';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Navigate to='products' />,
+	},
+	{
+		path: '/products',
+		element: <Products />,
+		errorElement: <UnexpectedError />,
+	},
+	{
+		path: '/favorites',
+		element: <Favorites />,
+		errorElement: <UnexpectedError />,
+	},
+	{
+		path: '*',
+		element: <PageNotFound />,
+	},
+]);
+
+export const Routes = () => {
+	return (
+		<RouterProvider router={router} />
+	);
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,7 @@
-export { };
+export * from './favorites';
+export * from './products';
+
+/* Errors */
+export * from './errors/404';
+export * from './errors/500';
 
