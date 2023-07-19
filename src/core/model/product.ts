d34b4ac4097ca7ae2d805c18d2c4434c47ee9df6
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-07-19 16:29:57 -0300

    ✨ feat: add product and category model

---
+++ src/core/model/product.ts
@@ -0,0 +1,21 @@
+import { Category } from './category';
+
+type Image = {
+	alt: string;
+	asset: {
+		url: string;
+	};
+};
+
+export type Product = {
+	id: string;
+	name: string;
+	shortDescription: string;
+	images: Image[];
+	category: Category;
+};
+
+export interface IProducts {
+	products: Product[];
+	categories: Category[];
+};
