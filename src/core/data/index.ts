e5bb73b1eb62ea5abacb2d9affbd877668e7fa44
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-07-20 10:37:29 -0300

    🌐 assets: add translated content files

--- src/core/data/index.ts
+++ src/core/data/index.ts
@@ -1,2 +1,4 @@
-export { default as DB_PRODUCTS } from './products.json';
+export { default as DB_PRODUCTS_EN } from './products-en.json';
+export { default as DB_PRODUCTS_ES } from './products-es.json';
+export { default as DB_PRODUCTS_PT } from './products-pt.json';
 
